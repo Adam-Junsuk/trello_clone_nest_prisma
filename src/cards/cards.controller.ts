b0870beaff445ba6417978b0b2f232642952c146
Adam Lee <adam.junsuk@gmail.com>
2023-09-20 22:17:46 +0900

    cards dto added

--- src/cards/cards.controller.ts
+++ src/cards/cards.controller.ts
@@ -9,33 +9,37 @@ import {
   Body,
 } from '@nestjs/common';
 import { CardsService } from './cards.service';
+import { CardDto } from './dto/cards.dto';
 
-@Controller('api/columns/:columnId/cards')
+@Controller('cards')
 export class CardsController {
   constructor(private readonly cardsService: CardsService) {}
 
   @Post()
-  createCard(@Param('columnId') columnId: number, @Body() data) {
-    return this.cardsService.createCard({ ...data, ColumnId: columnId });
+  async create(@Body() createCardDto: CardDto) {
+    return this.cardsService.createCard(createCardDto);
   }
 
-  @Get()
-  getAllCards(@Param('columnId') columnId: number) {
+  @Get(':columnId')
+  async getAll(@Param('columnId') columnId: number) {
     return this.cardsService.getAllCards(columnId);
   }
 
   @Get(':cardId')
-  getCardById(@Param('cardId') cardId: number) {
+  async getById(@Param('cardId') cardId: number) {
     return this.cardsService.getCardById(cardId);
   }
 
   @Put(':cardId')
-  updateCard(@Param('cardId') cardId: number, @Body() data) {
-    return this.cardsService.updateCard(cardId, data);
+  async update(
+    @Param('cardId') cardId: number,
+    @Body() updateCardDto: CardDto,
+  ) {
+    return this.cardsService.updateCard(cardId, updateCardDto);
   }
 
   @Delete(':cardId')
-  deleteCard(@Param('cardId') cardId: number) {
+  async delete(@Param('cardId') cardId: number) {
     return this.cardsService.deleteCard(cardId);
   }
 }
---
+++ src/cards/dto/cards.dto.ts
@@ -0,0 +1,8 @@
+export class CardDto {
+  name?: string;
+  description?: string;
+  color?: string;
+  order?: number;
+  ColumnId?: number;
+  dueDate?: Date;
+}
