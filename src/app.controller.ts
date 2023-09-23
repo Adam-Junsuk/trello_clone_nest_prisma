9452f334ff9b174cf85517f1712d907afab89c06
erickimme <jaeyeonkim0919@gmail.com>
2023-09-23 19:38:17 +0900

    app.controller modify path from 'google' to ''

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -2,15 +2,15 @@ import { Controller, Get, Render, Req, UseGuards } from '@nestjs/common';
 import { AppService } from './app.service';
 import { AuthGuard } from '@nestjs/passport';
 
-@Controller('google')
+@Controller('')
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
+  @Get('google')
   @UseGuards(AuthGuard('google'))
   async googleAuth(@Req() req) {}
 
-  @Get('redirect')
+  @Get('google/redirect')
   @UseGuards(AuthGuard('google'))
   googleAuthRedirect(@Req() req){
     return this.appService.googleLogin(req)
